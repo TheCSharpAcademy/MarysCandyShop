5b8a69ac31606648ecb02fbcd6be27b7a602e54f
Pablo <thecsharpacademy@gmail.com>
2023-09-02 08:36:22 +1000

    Basic Data Types

--- MarysCandyShop/Program.cs
+++ MarysCandyShop/Program.cs
@@ -1,4 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿// Most common data types in C#
 Console.WriteLine("Mary's Candy Shop");
+Console.WriteLine("---------------------------------");
+Console.WriteLine("Today's date: " + DateTime.Now);
+Console.WriteLine("Days since opening: " + 1);
+Console.WriteLine("Today's profit: " + 5.5m + "$");
+Console.WriteLine("Today's target achieved: " + false);
+Console.WriteLine("---------------------------------");
+Console.WriteLine("Choose one option:");
+Console.WriteLine('V' + " to view products");
+Console.WriteLine('A' + " to add product");
+Console.WriteLine('D' + " to delete product");
+Console.WriteLine('U' + " to update product");
 Console.ReadLine();
