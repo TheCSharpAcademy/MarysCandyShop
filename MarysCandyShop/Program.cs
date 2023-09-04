b527614e80742d916542f6a31e45a775f660cb52
Pablo <thecsharpacademy@gmail.com>
2023-09-04 19:52:19 +1000

    var; dotnet class library; reassigning variables

--- MarysCandyShop/Program.cs
+++ MarysCandyShop/Program.cs
@@ -1,11 +1,11 @@
-﻿// Variables
-string title = "Mary's Candy Shop";
-string divide = "---------------------------------";
-DateTime dateTime = DateTime.Now;
-int daysSinceOpening = 1;
-decimal todaysProfit = 5.5m;
-bool targetAchieved = false;
-string menu = "Choose one option:\n"
+﻿ // Variables
+var title = "Mary's Candy Shop";
+var divide = "---------------------------------";
+var dateTime = DateTime.Now;
+var daysSinceOpening = 1;
+var todaysProfit = 5.5m;
+var targetAchieved = false;
+var menu = "Choose one option:\n"
     + 'V' + " to view products\n"
     + 'A' + " to add product\n"
     + 'D' + " to delete product\n"
@@ -20,3 +20,24 @@ Console.WriteLine("Today's target achieved: " + targetAchieved);
 Console.WriteLine(divide);
 Console.WriteLine(menu);
 Console.ReadLine();
+
+Console.WriteLine(divide);
+Console.WriteLine(divide);
+
+title = title.ToUpper();
+divide = divide.Insert(0, "#########");
+dateTime = dateTime.Date;
+daysSinceOpening = int.Parse("2");
+todaysProfit = 10.9m;
+targetAchieved = true;
+
+Console.WriteLine(title);
+Console.WriteLine(divide);
+Console.WriteLine("Today's date: " + dateTime);
+Console.WriteLine("Days since opening: " + daysSinceOpening);
+Console.WriteLine("Today's profit: " + todaysProfit + "$");
+Console.WriteLine("Today's target achieved: " + targetAchieved);
+Console.WriteLine(divide);
+Console.WriteLine(menu);
+Console.ReadLine();
+
