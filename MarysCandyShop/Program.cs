735bfd94d87277b5ddfdf10086dcf1aaa00cf68d
Pablo <thecsharpacademy@gmail.com>
2023-09-06 10:35:10 +1000

    Running header and menu inside loop

--- MarysCandyShop/Program.cs
+++ MarysCandyShop/Program.cs
@@ -1,17 +1,46 @@
-﻿ // Variables
-var title = "Mary's Candy Shop";
-var divide = "---------------------------------";
-var dateTime = DateTime.Now;
-var daysSinceOpening = 1;
-var todaysProfit = 5.5m;
-var targetAchieved = false;
-var menu = "Choose one option:\n"
-    + 'V' + " to view products\n"
-    + 'A' + " to add product\n"
-    + 'D' + " to delete product\n"
-    + 'U' + " to update product\n";
+﻿var isMenuRunning = true;
 
-Console.WriteLine(@$"{title}
+while (isMenuRunning)
+{
+    PrintHeader();
+    RunMenu();
+    Console.WriteLine("Press Any Key To Go Back to Menu");
+    Console.ReadLine();
+    Console.Clear();
+};
+
+
+void AddProduct(string message)
+{
+    Console.WriteLine(message);
+}
+
+void DeleteProduct(string message)
+{
+    Console.WriteLine(message);
+}
+
+void UpdateProduct(string message)
+{
+    Console.WriteLine(message);
+}
+
+void ViewProducts(string message)
+{
+    Console.WriteLine(message);
+}
+
+void PrintHeader()
+{
+    var title = "Mary's Candy Shop";
+    var divide = "---------------------------------";
+    var dateTime = DateTime.Now;
+    var daysSinceOpening = 1;
+    var todaysProfit = 5.5m;
+    var targetAchieved = false;
+    string menu = GetMenu();
+
+    Console.WriteLine(@$"{title}
 {divide}
 Today's date: {dateTime}
 Days since opening: {daysSinceOpening}
@@ -19,26 +48,42 @@ Today's profit: {todaysProfit}$
 Today's target achieved: {targetAchieved}
 {divide}
 {menu}");
-
-var usersChoice = Console.ReadLine().Trim().ToUpper();
-
-switch (usersChoice)
-{
-    case "A":
-        Console.WriteLine("User chose A");
-        break;
-    case "D":
-        Console.WriteLine("User chose D");
-        break;
-    case "V":
-        Console.WriteLine("User chose V");
-        break;
-    case "U":
-        Console.WriteLine("User chose U");
-        break;
-    default:
-        Console.WriteLine("Invalid choice. Please choose one of the above");
-        break;
 }
 
+string GetMenu()
+{
+    return "Choose one option:\n"
+        + 'V' + " to view products\n"
+        + 'A' + " to add product\n"
+        + 'D' + " to delete product\n"
+        + 'U' + " to update product\n"
+        + 'Q' + " to quit program\n";
+}
 
+void RunMenu()
+{
+    var usersChoice = Console.ReadLine().Trim().ToUpper();
+
+    switch (usersChoice)
+    {
+        case "A":
+            AddProduct("User chose A");
+            break;
+        case "D":
+            DeleteProduct("User chose D");
+            break;
+        case "V":
+            ViewProducts("User chose V");
+            break;
+        case "U":
+            UpdateProduct("User chose U");
+            break;
+        case "Q":
+            Console.WriteLine("Goodbye");
+            isMenuRunning = false;
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please choose one of the above");
+            break;
+    }
+}
