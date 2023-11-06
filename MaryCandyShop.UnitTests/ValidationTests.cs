5ff893b48e9e2f25ebe17fc6e8b1362d287a4c9c
Pablo <thecsharpacademy@gmail.com>
2023-11-06 14:23:34 +1000

    Validation tests

---
+++ MaryCandyShop.UnitTests/ValidationTests.cs
@@ -0,0 +1,70 @@
+using MarysCandyShop;
+
+namespace MaryCandyShop.UnitTests
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void WhenStringIsValidReturnTrue()
+        {
+            var stringInput = "Test Chocolate Bar";
+            var result = Validation.IsStringValid(stringInput);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("string with more than 20 characters")]
+        public void WhenStringIsNotValidReturnFalse(string testString)
+        {
+            var result = Validation.IsStringValid(testString);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("20")]
+        [InlineData("20.5")]
+        public void WhenPriceIsValidReturnTrue(string testPrice)
+        {
+            var result = Validation.IsPriceValid(testPrice);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("-1")]
+        [InlineData("10000")]
+        [InlineData("84632846324836248732674832")]
+        public void WhenPriceIsNotValidReturnFalse(string testPrice)
+        {
+            var result = Validation.IsPriceValid(testPrice);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("20")]
+        public void WhenCocoaIsValidReturnTrue(string testCocoa)
+        {
+            var result = Validation.IsCocoaValid(testCocoa);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("-1")]
+        [InlineData("100")]
+        [InlineData("84632846324836248732674832")]
+        [InlineData("20.5")]
+        public void WhenCocoaIsNotValidReturnFalse(string testCocoa)
+        {
+            var result = Validation.IsCocoaValid(testCocoa);
+
+            Assert.False(result.IsValid);
+        }
+    }
+}
--- MarysCandyShop/Models/ValidationResponse.cs
+++ MarysCandyShop/Models/ValidationResponse.cs
@@ -1,18 +1,18 @@
 ﻿namespace MarysCandyShop.Models;
 
-internal class ValidationResponse
+public class ValidationResponse
 {
-    internal bool IsValid { get; set; }
-    internal string ErrorMessage { get; set; }
+    public bool IsValid { get; set; }
+    public string ErrorMessage { get; set; }
 
 }
 
-internal class CocoaValidationResponse : ValidationResponse
+public class CocoaValidationResponse : ValidationResponse
 {
-    internal int CocoaPercentage { get; set; }
+    public int CocoaPercentage { get; set; }
 }
 
-internal class PriceValidationResponse : ValidationResponse
+public class PriceValidationResponse : ValidationResponse
 {
-    internal decimal Price { get; set; }
+    public decimal Price { get; set; }
 }
